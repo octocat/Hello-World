d8a0599344603dfe44fa8d54071d6b21b53c0b0c
Ken Reed <ken@kmreed.net>
2014-05-17 07:59:25 -0700

    Add a Perl implementation
    Just playing with the whole GitHub process.

---
+++ HelloWorld.pl
@@ -0,0 +1,17 @@
+# This will print hello world.
+# Not very sophisticated; I'll give you that.
+
+use strict;
+sub hello_world;
+sub so_what;
+
+hello_world();
+so_what();
+
+sub hello_world {
+    print "Hello World!\n";
+}
+
+sub so_what {
+    print "Whatever!\n";
+}
